61706c788ed8902fdf955ed35692807864a9ec22
Yuvraj Chauhan <yuvraj.chauhan1510@gmail.com>
2026-08-15 14:29:39 +0530

    Save Member 4 alerts and Firebase changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'core/theme/app_theme.dart';
 import 'features/splash/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
